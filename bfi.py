ac5116475cef55c8dad4270b1a7cf66d75c859a5
tyru <tyru.exe@gmail.com>
2010-07-01 03:50:13 +0900

    Implement "unroll_loop".

--- bfi.py
+++ bfi.py
@@ -58,6 +58,9 @@ class MismatchParenthesis(Exception):
 	def __str__(self):
 		return self.msg
 
+class InternalError(Exception):
+	def __str__(self):
+		return "Sorry, internal error."
 
 
 
@@ -153,6 +156,7 @@ class BFMachine(object):
 	__slots__ = [
 		'__src',
 		'__optable',
+		'__defaultheaplen',
 		'heap',
 		'heapindex',
 		'ops',
@@ -162,6 +166,7 @@ class BFMachine(object):
 	
 	def __init__(self, file, **kwargs):
 		self.__src = ''.join(file.readlines())
+		
 		# Set user-defined values.
 		for optoken in kwargs.get('tokens', []):
 			if isinstance(kwargs[token], list):
@@ -171,25 +176,34 @@ class BFMachine(object):
 			apply(self.__optable.settoken, args)
 		
 		self.__optable = BFOpsTable()
-		# TODO: Use bytearray not list
-		self.heap = [0 for times in range(kwargs.get('heaplen', 30))]
-		self.heapindex = 0
+		
+		self.__defaultheaplen = kwargs.get('heaplen', 30)
+		self.clear_heap()
+		
 		self.ops = None
 		self.__opsindex = None
+		
 		self.__compile_options = {'unroll_loop': 0}
 		if 'compile_with' in kwargs:
 			self.__compile_options.update(kwargs['compile_with'])
+	
+	def clear_heap(self):
+		# TODO: Use bytearray not list
+		self.heap = [0 for times in range(self.__defaultheaplen)]
+		self.heapindex = 0
 	
 	def compile(self):
 		if self.ops is not None:
 			return
 		self.ops = []
+		has_loop = 0
 		loop_paren_count = 0
 		for c in self.__src:
 			if self.__optable.hasop(c):
 				op = self.__optable.getop(c)
 				self.ops.append(op)
 				if op == BFOpsTable.op_loopbegin:
+					has_loop = 1
 					loop_paren_count += 1
 				elif op == BFOpsTable.op_loopend:
 					loop_paren_count -= 1
@@ -198,20 +212,56 @@ class BFMachine(object):
 		if loop_paren_count != 0:
 			if loop_paren_count > 0:
 				raise MismatchParenthesis("No right ']' correspond to '['.")
-		if self.__compile_options.get('unroll_loop', 0):
-			self.unroll_loop()
 		self.__opsindex = 0
+		if has_loop and self.__compile_options.get('unroll_loop', 0):
+			self.unroll_loop()
 	
 	def unroll_loop(self):
-		for op in self.ops:
+		calling_op = set([BFOpsTable.op_incptr, BFOpsTable.op_decptr, BFOpsTable.op_incvalue, BFOpsTable.op_decvalue])
+		while hasidx(self.ops, self.__opsindex):
+			op = self.ops[self.__opsindex]
 			if op == BFOpsTable.op_output or op == BFOpsTable.op_input:
 				self.__opsindex += 1
-			elif op == BFOpsTable.op_loopbegin:
-				pass    # TODO
-			elif op == BFOpsTable.op_loopend:
-				pass    # TODO
-			else:
+			elif op in calling_op:
 				self.call_op()
+			elif op == BFOpsTable.op_loopbegin:
+				unroll_times = 0
+				loopbegin_index = self.__opsindex
+				loopend_index = -1
+				while hasidx(self.ops, self.__opsindex):
+					if self.ops[self.__opsindex] == BFOpsTable.op_loopbegin:
+						if self.heap[self.heapindex] == 0:
+							if unroll_times == 0:
+								while op != BFOpsTable.op_loopend:
+									self.ops.pop(self.__opsindex)
+									op = self.ops[self.__opsindex]
+								# Remove also `BFOpsTable.op_loopend`.
+								self.ops.pop(self.__opsindex)
+							else:
+								# Remove op_loopend, op_loopbegin.
+								self.ops.pop(loopend_index)
+								self.ops.pop(loopbegin_index)
+								# Unroll body ops.
+								unroll_ops = self.ops[loopbegin_index:loopend_index-1]
+								unroll_ops_num = len(unroll_ops)
+								self.ops[loopbegin_index:loopend_index-1] = unroll_ops * unroll_times
+								self.__opsindex += unroll_ops_num
+								break
+						else:
+							unroll_times += 1
+							self.call_op()    # Let `op_loopbegin` advance `self.__opsindex`.
+					else:
+						if self.ops[self.__opsindex] == BFOpsTable.op_loopend \
+						and loopend_index == -1:
+							loopend_index = self.__opsindex
+						self.call_op()
+			elif op == BFOpsTable.op_loopend:
+				raise InternalError("unroll_loop(): `op` must NOT be `BFOpsTable.op_loopend`.".format(op.func_name))
+			else:
+				raise InternalError("Unknown method '{0}'.".format(op.func_name))
+		# Initialize attributes for real `run()`!
+		self.clear_heap()
+		self.__opsindex = 0
 	
 	def call_op(self):
 		# Do not let ops change `self.__opsindex` not by return value.
